7e3edb9bd84107b6d09ade50be0635342a557b40
EdwardHoward <edwardhoward11@gmail.com>
2021-10-06 23:37:06 +0000

    Add transaction mocks

--- webapp/mocks/transactions-data.js
+++ webapp/mocks/transactions-data.js
@@ -2,8 +2,15 @@ const transactions = [
   {
     'id': '5d5c1f747e01cd704f18f863',
     'user_id': 'employee4',
+    'user': {
+      'firstName': 'employee',
+      'lastName': '4'
+    },
     'description': 'cleaningsupplies',
     'merchant_id': 'walmart',
+    'merchant': {
+      'name': 'walmart'
+    },
     'debit': true,
     'credit': false,
     'amount': 150,
@@ -12,8 +19,15 @@ const transactions = [
   {
     'id': '5d5c1f747e01cd704f18f864',
     'user_id': 'employee3',
+    'user': {
+      'firstName': 'employee',
+      'lastName': '3'
+    },
     'description': 'refund',
     'merchant_id': 'walmart',
+    'merchant': {
+      'name': 'walmart'
+    },
     'debit': false,
     'credit': true,
     'amount': 250,
@@ -22,8 +36,15 @@ const transactions = [
   {
     'id': '5d5c1f747e01cd704f18f865',
     'user_id': 'employee5',
+    'user': {
+      'firstName': 'employee',
+      'lastName': '5'
+    },
     'description': 'refund',
     'merchant_id': 'walmart',
+    'merchant': {
+      'name': 'walmart'
+    },
     'debit': false,
     'credit': true,
     'amount': 100,
