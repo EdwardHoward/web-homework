56cf76279ae6f7306ff43c3ef7c0787ed0ba5a4b
EdwardHoward <edwardhoward11@gmail.com>
2021-10-06 23:44:57 +0000

    Add TxTableRow component

--- webapp/src/components/transactions/TxTableRow.js
+++ webapp/src/components/transactions/TxTableRow.js
@@ -2,17 +2,19 @@ import React from 'react'
 import { string, bool, number, shape } from 'prop-types'
 import TableCell from '@mui/material/TableCell'
 import TableRow from '@mui/material/TableRow'
+import { Link } from 'react-router-dom';
 
 const makeDataTestId = (transactionId, fieldName) => `transaction-${transactionId}-${fieldName}`
 
-export default function TxTableRow ({ data }) {
+export function TxTableRow ({ data }) {
   const { id, user, description, merchant, debit, credit, amount } = data
 
   return (
     <TableRow
+      data-testid='tx-table-row'
       sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
     >
-      <TableCell align='left' data-testid={makeDataTestId(id, 'id')}>{id}</TableCell>
+      <TableCell align='left' data-testid={makeDataTestId(id, 'id')}><Link to={`/transactions/${id}`}>{id}</Link></TableCell>
       <TableCell align='left' data-testid={makeDataTestId(id, 'userId')}>{`${user.firstName} ${user.lastName}`}</TableCell>
       <TableCell align='left' data-testid={makeDataTestId(id, 'description')}>{description}</TableCell>
       <TableCell align='left' data-testid={makeDataTestId(id, 'merchant')}>{merchant.name}</TableCell>
