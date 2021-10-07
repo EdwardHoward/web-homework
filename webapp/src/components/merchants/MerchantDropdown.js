7074daa18bb7fcfcf321ed6577bc16101bee253b
EdwardHoward <edwardhoward11@gmail.com>
2021-10-07 04:42:44 +0000

    Add Merchant Dropdown

---
+++ webapp/src/components/merchants/MerchantDropdown.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MenuItem, Select } from '@mui/material';
+import GetMerchants from '../../gql/merchants.gql'
+import { useQuery } from '@apollo/client';
+
+export function MerchantDropdown({ value, onChange }) {
+    const { loading, error, data = {} } = useQuery(GetMerchants)
+
+    if(!loading && !error) {
+        return (
+            <Select onChange={onChange} value={value}>
+                {data.merchants.map(merchant => (
+                    <MenuItem key={merchant.id} value={merchant.id}>{merchant.name}</MenuItem>
+                ))}
+            </Select>
+        )
+    }
+
+    return null;
+}
